875d62d48418eace20c766f3a309b00ce6ef86fd
pilgyu <pilgyu12@naver.com>
2018-11-29 10:29:44 +0900

    2018-11-29 Problem Solving

--- src/하나로_1251/Solution.java
+++ src/하나로_1251/Solution.java
@@ -1,54 +1,105 @@
 package ÇÏ³ª·Î_1251;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
-
 import java.util.Scanner;
 
 public class Solution {
 	
-	static class Node{
-		int x;
-		int y;
+	static class Node implements Comparable<Node>{
+		int p;
+		int s;
+		BigDecimal dist;
 		
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
+		public Node(int p, int s, BigDecimal dist) {
+			super();
+			this.p = p;
+			this.s = s;
+			this.dist = dist;
 		}
 		
+		@Override
+		public String toString() {
+			return "Node [p=" + p + ", s=" + s + ", dist=" + dist + "]";
+		}
+
+		@Override
+		public int compareTo(Node n) {
+			if(this.dist.compareTo(n.dist) == -1 ) {
+				return -1;
+			}
+			else if(this.dist.compareTo(n.dist) == 1 ) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+	
+	public static void makeset(int a) {
+		connect[a] = a;
+	}
+	
+	public static int findset(int s) {
+		if(connect[s] == s) {
+			return s;
+		}
+		return findset(connect[s]);
+	}
+	
+	public static void unionset(int p, int s) {
+		connect[findset(s)] = connect[p];
 	}
 
+	private static int[] connect;
+ 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int test = sc.nextInt();
 		for(int i = 1; i <= test; i++) {
 			int n = sc.nextInt();
-			int[] cal = new int[n * (n-1) / 2];
-			Node[] ar = new Node[n];
+			Node[] ar = new Node[n * (n-1) / 2];
+			int[] xar = new int[n];
+			int[] yar = new int[n];
+			connect = new int[n];
 			for(int j = 0; j < n; j++) {
-				int x = sc.nextInt();
-				int y = sc.nextInt();
-				Node no = new Node(x,y);
-				ar[j] = no;
+				xar[j] = sc.nextInt();
+				makeset(j);
 			}
-			double e = sc.nextDouble();
+			for(int j = 0; j < n; j++) {
+				yar[j] = sc.nextInt();
+			}
+			String e = sc.next();
 			int index = 0;
 			for(int j = 0; j < n; j++) {
 				for(int k = j+1; k < n; k++) {
-					cal[index++] = (ar[j].x - ar[k].x) + (ar[j].y - ar[k].y);
+					BigDecimal b1 = new BigDecimal(String.valueOf(Math.abs(xar[j] - xar[k])));
+					BigDecimal b2 = new BigDecimal(String.valueOf(Math.abs(yar[j] - yar[k])));
+					b1 = b1.multiply(b1);
+					b2 = b2.multiply(b2);
+					b1 = b1.add(b2);
+					Node no = new Node(j,k,b1);
+					ar[index++] = no;
 				}
 			}
-			for(int z = 0; z < cal.length;z++) {
-				System.out.println(cal[z]);
+			Arrays.sort(ar);
+			BigDecimal sum = new BigDecimal("0");
+			int count = 0;
+			for(int j = 0; j < n * (n-1) / 2; j++) {
+				if(count == n - 1) {
+					break;
+				}
+				if(findset(ar[j].p) == findset(ar[j].s)) {
+					continue;
+				}
+				unionset(ar[j].p, ar[j].s);
+				BigDecimal dis = new BigDecimal(String.valueOf(ar[j].dist));
+				sum = sum.add(dis);
+				count++;
 			}
-			Arrays.sort(cal);
-			int sum = 0;
-			for(int j = 0; j < n-1; j++) {
-				System.out.println(cal[j]);
-				sum += cal[j] * cal[j];
-			}
-			System.out.printf("#%d %d\n",i,(int)(sum*e));
+			sum = sum.multiply(new BigDecimal(e));
+			System.out.printf("#%d ",i);
+			System.out.println(sum.setScale(0, RoundingMode.HALF_UP));
 		}
-
 	}
-
 }
