766439310af6d96cfb44bd3cc864549c1a59d300
win10 <win10@DESKTOP-S5CG02G>
2018-12-01 01:22:14 +0900

    2018-12-01 Problem Solving

--- src/보급로_1249/Solution.java
+++ src/보급로_1249/Solution.java
@@ -1,108 +1,87 @@
 package º¸±Þ·Î_1249;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
+ 
 public class Solution {
-	
-	static class Node{
-		int x;
-		int y;
-		
-		public Node(int x, int y) {	
-			this.x = x;
-			this.y = y;
-		}
-		
-	}
-	
-	private static int[][] dist;
-	private static int[][] map;
-	private static boolean[][] check;
-	private static int n;
-	
-	public static void dijkstra(int x, int y) {
-		Node n = new Node(x,y);
-		Queue<Node> qu = new LinkedList<Node>();
-		qu.add(n);
-		dist[x][y] = 0;
-		while(!qu.isEmpty()) {
-			Node no = qu.poll();
-			if(check[no.x][no.y]) {
-				continue;
-			}
-			check[no.x][no.y] = true;
-			if(!check[no.x-1][no.y]) {
-				Node nw = new Node(no.x-1,no.y);
-				qu.add(nw);
-				if(dist[no.x-1][no.y] > map[no.x-1][no.y] + dist[no.x][no.y]) {
-					dist[no.x-1][no.y] = map[no.x-1][no.y] + dist[no.x][no.y];
-				}
-				
-			}
-			if(!check[no.x+1][no.y]) {
-				Node nw = new Node(no.x+1,no.y);
-				qu.add(nw);
-				if(dist[no.x+1][no.y] > map[no.x+1][no.y] + dist[no.x][no.y]) {
-					dist[no.x+1][no.y] = map[no.x+1][no.y] + dist[no.x][no.y];
-				}
-			
-			}
-			if(!check[no.x][no.y-1]) {
-				Node nw = new Node(no.x,no.y-1);
-				qu.add(nw);
-				if(dist[no.x][no.y-1] > map[no.x][no.y-1] + dist[no.x][no.y]) {
-					dist[no.x][no.y-1] = map[no.x][no.y-1] + dist[no.x][no.y];
-				}
-			
-			}
-			if(!check[no.x][no.y+1]) {
-				Node nw = new Node(no.x,no.y+1);
-				qu.add(nw);
-				if(dist[no.x][no.y+1] > map[no.x][no.y+1] + dist[no.x][no.y]) {
-					dist[no.x][no.y+1] = map[no.x][no.y+1] + dist[no.x][no.y];
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int test = sc.nextInt();
-		for (int i = 1; i <= test; i++) {
-			n = sc.nextInt();
-			map = new int[n+2][n+2];
-			dist = new int[n+2][n+2];
-			check = new boolean[n+2][n+2];
-			for (int j = 0; j < n + 2; j++) {
-				String[] s;
-				if (j == 0 || j == n + 1) {
-					Arrays.fill(check[j], true);
-					Arrays.fill(dist[j], 100000);
-					continue;
-				} else {
-					s = sc.next().split("");
-				}
-				for (int k = 0; k < n + 2; k++) {
-					if (k == 0 || k == n + 1) {
-						check[j][k] = true;
-						dist[j][k] = 100000;
-						continue;
-					}
-					dist[j][k] = 100000;
-					map[j][k] = Integer.parseInt(s[k - 1]);
-				}
-			}
-			dijkstra(1, 1);
-			for(int j = 0; j < n+2; j++) {
-				for(int k = 0 ; k < n+2; k++) {
-					System.out.print(dist[j][k] + " ");
-				}
-				System.out.println();
-			}
-			System.out.printf("#%d %d\n",i,dist[n][n]);
-		}
-	}
+     
+    static class Node{
+        int x;
+        int y;
+         
+        public Node(int x, int y) { 
+            this.x = x;
+            this.y = y;
+        }
+         
+    }
+     
+    private static int[][] dist;
+    private static int[][] map;
+    private static boolean[][] check;
+    private static int n;
+     
+    public static void dijkstra(int x, int y) {
+        Node nx = new Node(x,y);
+        Queue<Node> qu = new LinkedList<Node>();
+        qu.add(nx);
+        dist[x][y] = 0;
+        while(!qu.isEmpty()) {
+            Node no = qu.poll();
+            check[no.x][no.y] = true;
+            for(int j = 0; j < 4; j++) {
+                if(j == 0) {
+                    if(no.x >= 1 && dist[no.x-1][no.y] > map[no.x-1][no.y] + dist[no.x][no.y]) {
+                        dist[no.x-1][no.y] = map[no.x-1][no.y] + dist[no.x][no.y];
+                        Node nw = new Node(no.x-1,no.y);
+                        check[no.x-1][no.y] = true;
+                        qu.add(nw);
+                    }
+                }
+                else if(j == 1) {
+                    if(no.x  < n - 1 && dist[no.x+1][no.y] > map[no.x+1][no.y] + dist[no.x][no.y]) {
+                        dist[no.x+1][no.y] = map[no.x+1][no.y] + dist[no.x][no.y];
+                        Node nw = new Node(no.x+1,no.y);
+                        qu.add(nw);     
+                    }
+                }
+                else if(j == 2) {
+                    if(no.y < n - 1 && dist[no.x][no.y+1] > map[no.x][no.y+1] + dist[no.x][no.y]) {
+                        dist[no.x][no.y+1] = map[no.x][no.y+1] + dist[no.x][no.y];
+                        Node nw = new Node(no.x,no.y+1);
+                        qu.add(nw);
+                    }
+                }
+                else if(j == 3) {
+                    if(no.y >= 1 && dist[no.x][no.y-1] > map[no.x][no.y-1] + dist[no.x][no.y]) {
+                        dist[no.x][no.y-1] = map[no.x][no.y-1] + dist[no.x][no.y];
+                        Node nw = new Node(no.x,no.y-1);
+                        qu.add(nw);     
+                    }
+                }
+            }
+        }
+    }
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (int i = 1; i <= test; i++) {
+            n = sc.nextInt();
+            map = new int[n][n];
+            dist = new int[n][n];
+            check = new boolean[n][n];
+            for (int j = 0; j < n; j++) {
+                String[] s = sc.next().split("");
+                for (int k = 0; k < n; k++) {
+                    dist[j][k] = 100000;
+                    map[j][k] = Integer.parseInt(s[k]);
+                }
+            }
+ 
+            dijkstra(0, 0);
+            System.out.printf("#%d %d\n",i,dist[n-1][n-1]);
+        }
+    }
 }
