c8d01d93a5e9b9553d26bcafbaa96ec16118df1d
pilgyu12@naver.com <pilgyu12@naver.com>
2019-01-06 23:21:39 +0900

    2019-01-06 problem solving

--- src/백만장자프로젝트_1859/Solution.java
+++ src/백만장자프로젝트_1859/Solution.java
@@ -2,45 +2,21 @@ package 백만장자프로젝트_1859;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.LinkedList;
-=======
 import java.util.Scanner;
->>>>>>> branch 'master' of https://github.com/pilgyumin/sw-expert-academy-PS-.git
 
 public class Solution {
-<<<<<<< HEAD
-	
-	static ArrayList<Integer> ar;
-	static BigInteger sum;
-=======
 
->>>>>>> branch 'master' of https://github.com/pilgyumin/sw-expert-academy-PS-.git
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 	
 			int test = sc.nextInt();
 			for(int i = 1; i <= test; i++) {
-<<<<<<< HEAD
-				int num = Integer.parseInt(br.readLine());
-				ar = new ArrayList<Integer>();
-				String[] s  = br.readLine().split(" ");
-=======
 				int num = sc.nextInt();
 				ArrayList<Integer> ar = new ArrayList<Integer>();
->>>>>>> branch 'master' of https://github.com/pilgyumin/sw-expert-academy-PS-.git
 				for(int j = 0; j < num; j++) {
-<<<<<<< HEAD
-					ar.add(Integer.parseInt(s[j]));
-=======
 					ar.add(sc.nextInt());
->>>>>>> branch 'master' of https://github.com/pilgyumin/sw-expert-academy-PS-.git
 				}
-<<<<<<< HEAD
-				sum = new BigInteger("0");
-=======
 				BigInteger sum = new BigInteger("0");
->>>>>>> branch 'master' of https://github.com/pilgyumin/sw-expert-academy-PS-.git
 				int cal = ar.get(num-1);
 				for(int j = num - 1; j >= 0; j--) {
 					if(cal > ar.get(j)) {
--- src/이미지유사도검사_1264/Solution.java
+++ src/이미지유사도검사_1264/Solution.java
@@ -1,86 +1,37 @@
 package 이미지유사도검사_1264;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 
 public class Solution {
-	
-	static int[] ar;
-	static int[] parent;
-	static int[] idx;
-	static int[] list;
-	static ArrayList<Integer> ans;
-	
-	private static int lowerbound(int s, int e, int n) {
-		int start = s, end = e, num = n, mid;
-		while(start <= end) {
-			mid = (start + end) / 2;
-			if(list[mid] >= num) {
-				end = mid - 1;
-				
-			}
-			else {
-				start = mid + 1;
-			}
-		}
-		return start;
-	}
-	
-	private static int arr(int i) {
-		if(parent[i] < 0) {
-			return ar[idx[i]];
-		}
-		int x = arr(parent[i]);
-		ans.add(ar[x]);
-		return ar[idx[i]];
-	}
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		ar = new int[num];
-		parent = new int[num];
-		idx = new int[num];
-		list = new int[num];
-		ans = new ArrayList<>();
-		for(int i = 0; i < num; i++) {
-			ar[i] = sc.nextInt();
-		}
-		Arrays.fill(parent, -1);
-		
-		list[0] = ar[0];
-		idx[0] = 0;
-		int size = 1;
-		
-		for(int i = 1; i < num; i++) {
-			if(ar[i] >= list[size-1]) {
-				list[size] = ar[i];
-				parent[i] = idx[size-1];
-				idx[size] = i;
-				size++;
-			}
-			else {
-				int id = lowerbound(0,size-1,ar[i]);
-				list[id] = ar[i];
-				idx[id] = i;
-				if(id > 0) {
-					parent[i] = idx[id-1];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int test = Integer.parseInt(br.readLine());
+			for (int i = 1; i <= test; i++) {
+				
+				int num = Integer.parseInt(br.readLine().trim());
+				
+				int[][] map = new int[num+1][num+1];
+				
+				String[] x = br.readLine().split("");
+				String[] y = br.readLine().split("");
+				for(int j = 1; j < num+1; j++) {
+					for(int k = 1; k < num+1; k++) {
+						if(x[j-1].equals(y[k-1])) {
+							map[j][k] = map[j-1][k-1] + 1;
+						}
+						else {
+							map[j][k] = Math.max(map[j-1][k], map[j][k-1]);
+						}
+					}
 				}
+				System.out.printf("#%d %.2f\n",i,map[num][num]/(double)num * 100);
 			}
+		} catch (NumberFormatException | IOException e) {
 		}
-		int x = idx[size-1];
-		while(x != -1) {
-			ans.add(ar[x]);
-			x = parent[x];
-		}
-
-		Collections.sort(ans);
-		for(int i = 0; i < ans.size(); i++) {
-			System.out.print(ans.get(i) + " ");
-		}
-
 	}
-
 }
